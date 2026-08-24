e0702d737d6698640033fd48485350630df24ef3
geralt14 <wmkkkk@126.com>
2026-08-24 15:07:49 +0100

    update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="enzywizard-aaprops",
-    version=__version__,                     # dynamically read from version.py (1.0.1)
+    version=__version__,                     # dynamically read from version.py
     author="bioinfbrad",
     description=(
         "Calculate amino acid residue properties from a cleaned protein structure "
